6065804cf630147024438d423a0ede44bae112c2
DianRuth1208 <dianaruthsantos@gmail.com>
2019-08-30 13:23:06 -0500

    version 2.0

--- proyecto.cpp
+++ proyecto.cpp
@@ -1,20 +1,45 @@
 #include <iostream>
+#include <conio.h>
 using namespace std;
-struct Animal{
+
+
+struct album {
+	char alb[20];
+};
+
+struct cancion{
 	char nombre[20];
-	int edad;
-} animal1, animal2;
+	int codigo;
+	char genero [20];
+	struct album al;
+};
 
 int main (int argc, char** argv) {
+	struct cancion C1[5];
+	struct cancion*apuntador;
 	
+	apuntador=&C1[0];
 	
+	for (int i=0; i<1;i++)
+	{
 	cout<<"Nombre: ";
-	cin.getline(animal2.nombre, 20, '\n' );
-	cout<<"Edad: ";
-	cin>>animal2.edad;
+	cin.getline(C1[i].nombre, 20, '\n' );
+	cout<<"Codigo: ";
+	cin>>C1[i].codigo;
+	cout<<"Genero";
+	cin>>C1[i].genero;
+	cout<<"Nombre del album: ";
+	fflush(stdin);
+	cin.getline(C1[i].al.alb, 20, '\n');	
+	}
 	
-	cout<<animal2.nombre<<endl;
-	cout<<animal2.edad<<endl;
-	system("pause");
+	
+	cout<<endl<<"Nombre guardado: "<<apuntador->nombre;
+	cout<<endl<<"Codigo guardado: "<<apuntador->codigo;
+	cout<<endl<<"Genero guardado: "<<apuntador->genero;
+	cout<<endl<<"Album guardado: "<<apuntador->al.alb;
+	
+	
+	getch();
 	return 0;
 }
