e7e839d346401ef123d170fa652ddbc2939def63
DianRuth1208 <dianaruthsantos@gmail.com>
2019-09-25 13:04:59 -0500

    version 3.0

--- proyecto.cpp
+++ proyecto.cpp
@@ -1,45 +1,70 @@
-#include <iostream>
-#include <conio.h>
+#include<iostream>
+#include<stdio.h>
+#include<string.h>
+
 using namespace std;
 
+struct persona{
+    char nombre[20];
+    int suscripcion;
+    char revista[20];
+    char eliminar[20];
 
-struct album {
-	char alb[20];
 };
 
-struct cancion{
-	char nombre[20];
-	int codigo;
-	char genero [20];
-	struct album al;
-};
+int main(){
 
-int main (int argc, char** argv) {
-	struct cancion C1[5];
-	struct cancion*apuntador;
-	
-	apuntador=&C1[0];
-	
-	for (int i=0; i<1;i++)
-	{
-	cout<<"Nombre: ";
-	cin.getline(C1[i].nombre, 20, '\n' );
-	cout<<"Codigo: ";
-	cin>>C1[i].codigo;
-	cout<<"Genero";
-	cin>>C1[i].genero;
-	cout<<"Nombre del album: ";
-	fflush(stdin);
-	cin.getline(C1[i].al.alb, 20, '\n');	
-	}
-	
-	
-	cout<<endl<<"Nombre guardado: "<<apuntador->nombre;
-	cout<<endl<<"Codigo guardado: "<<apuntador->codigo;
-	cout<<endl<<"Genero guardado: "<<apuntador->genero;
-	cout<<endl<<"Album guardado: "<<apuntador->al.alb;
-	
-	
-	getch();
-	return 0;
+    struct persona P1[10];
+    struct persona *apuntador;
+    apuntador=P1;
+    int opcion,contador;
+    char eliminar[20];
+    for(int i=0; i<10; i++){
+        P1[i].nombre[0]='0';
+        cout<<"\t.:::Se muestra el menu de una revista:::.\n";
+        cout<<"1. Dar de alta\n";
+        cout<<"2. Dar de baja\n";
+        cout<<"3. Salir\n";
+        cin>>opcion;
+    switch (opcion){
+        case 1:
+            for(int i=0;i<10; i++){
+                if(P1[i].nombre[0]=='n'){
+                    cout<<"Ingresa el nombre: ";
+                    fflush(stdin);
+                    cin.getline(P1[i].nombre,20,'\0');
+                    cout<<"Ingresa el tiempo de suscripcion en meses: ";
+                    cin>>P1[i].suscripcion;
+                    cout<<"Ingresa el nombre de la revista: ";
+                    fflush(stdin);
+                    cin.getline(P1[i].revista,20,'\0');
+                    cout<<'\0';
+                    break;
+                    }else
+                    cout<<"El "<<i+1<<" espacio esta ocupado"<<endl;
+        }break;
+            case 2:
+                cout<<"Ingrese el nombre del usuario que desea dar de baja: ";fflush(stdin);
+                cin.getline(eliminar,20,'\0');
+                for(int i=0; i<10; i++){
+                        contador=strcmp(apuntador->nombre,eliminar);
+                if (contador!=0)
+                    apuntador++;
+                else{
+                    P1[i].nombre[0]='0';
+                    cout<<"El usuario ha sido eliminado";
+                    break;
+                }
+            }
+            break;
+            default:
+                break;
+
+}
+ }
+
+
+    return 0;
+
+
 }
